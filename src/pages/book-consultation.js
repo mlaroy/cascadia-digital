34571046a492cbe5185a7b46f1345adb9e9e108a
Michael LaRoy <mike.laroy@gmail.com>
2020-12-31 14:48:17 -0800

    rename booking form

--- src/pages/book-consultation.js
+++ src/pages/book-consultation.js
@@ -139,7 +139,7 @@ const ContactPage = (props) => {
         <Section>
           {renderContactLink(hasJS)}
           {hasJS && (
-            <form onSubmit={handleContactSubmit} className="mx-auto max-w-lg" method="post" data-netlify="true" data-netlify-honeypot="bot-field" name="consultation">
+            <form onSubmit={handleContactSubmit} className="mx-auto max-w-lg" method="post" data-netlify="true" name="booking">
               <p className="mb-8 text-base md:text-2xl">
                 It's important for me to know as many details as possible regarding your project.
                 This helps me have a clear picture of what you are looking to accomplish,
@@ -147,7 +147,8 @@ const ContactPage = (props) => {
               </p>
               <input
                 type="hidden"
-                name="bot-field"
+                value="booking"
+                name="form-name"
               />
               <input type="text" className="hidden" value={honeypot} onChange={e => setHoneypot(e.target.value) } />
               <label className="mb-8 block text-sm">
